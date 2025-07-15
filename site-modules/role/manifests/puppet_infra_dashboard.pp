e9a47f8d8450be67b2da6c9430d65b8e40c9e98a
trips4 <dennis.triplo@perforce.com>
2025-07-15 13:09:35 -0500

    PDK Support for Role Module (#3)
    * PDK Enabled site-modules/role
    
    * new role for infra dashboard

---
+++ site-modules/role/manifests/puppet_infra_dashboard.pp
@@ -0,0 +1,9 @@
+# @summary Manage the Puppet infrastructure dashboard
+#
+# A description of what this class does
+#
+# @example
+#   include role::puppet_infra_dashboard
+class role::puppet_infra_dashboard {
+  include puppet_operational_dashboards
+}
